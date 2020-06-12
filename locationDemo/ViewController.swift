fafd742b54528c4c6632cdd45238f4a8e2f407c9
monikasharma26 <monikasharma26081991@gmail.com>
2020-06-12 17:03:13 -0400

    Initial Commit

---
+++ locationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  locationDemo
+//
+//  Created by S@i on 2020-06-12.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
